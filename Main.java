9c2a32fef1b138a2ae85ec3bf1dc9ad9a29a6b94
Bader Al-Hamdan <bader.z.alhamdan@gmail.com>
2023-05-23 20:55:26 +0300

    yes

--- Main.java
+++ Main.java
@@ -101,7 +101,7 @@ public class Main
             return removedItem;
         }
     }
-
+//123123123123123123123213123123123
     public static void main(String[] args) throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         String[] line = in.readLine().split("\\s+");
